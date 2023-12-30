a2d557ffcc56b8bdd1846cd75f8a3376774fa2e0
cgisky1980 <new448885@gmail.com>
2023-12-30 21:54:59 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,7 +14,9 @@ use std::{
     path::{Path, PathBuf},
     sync::Arc,
     time::Duration,
+    
 };
+
 
 use anyhow::Result;
 use axum::{
@@ -28,6 +30,7 @@ use itertools::Itertools;
 use memmap2::Mmap;
 use run::RuntimeUntyped;
 use serde::{Deserialize, Serialize};
+use tokio::task;
 use tokio::sync::{Mutex, RwLock};
 use tower_http::{cors::CorsLayer, services::ServeDir};
 use web_rwkv::{
@@ -554,7 +557,126 @@ pub async fn request_info_stream(
     }
 }
 
-pub fn tauri_main() {
+
+
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long, short, value_name = "FILE")]
+    config: Option<PathBuf>,
+    #[arg(long, short)]
+    ip: Option<Ipv4Addr>,
+    #[arg(long, short, default_value_t = 65530)]
+    port: u16,
+}
+
+
+async fn main2() -> Result<(),anyhow::Error> {
+
+    simple_logger::SimpleLogger::new()
+        .with_level(log::LevelFilter::Warn)
+        .with_module_level("ai00_server", log::LevelFilter::Trace)
+        .init()
+        .unwrap();
+
+    let args = Args::parse();
+    let (sender, receiver) = flume::unbounded::<ThreadRequest>();
+    tokio::task::spawn_blocking(move || model_route(receiver));
+
+    {
+        let path = args
+            .config
+            .clone()
+            .unwrap_or("assets/configs/Config.toml".into());
+        log::info!("reading config {}...", path.to_string_lossy());
+
+        let request = load_config(path).expect("load config failed").into();
+        let _ = sender.send(ThreadRequest::Reload {
+            request,
+            sender: None,
+        });
+    }
+
+    let serve_path = {
+        let path = tempfile::tempdir()
+            .expect("create temp dir failed")
+            .into_path();
+        load_web("assets/www/index.zip", &path).expect("load frontend failed");
+        path
+    };
+
+
+
+    // create `assets/www/plugins` if it doesn't exist
+    if !Path::new("assets/www/plugins").exists() {
+        fs::create_dir("assets/www/plugins").expect("create plugins dir failed");
+    }
+
+    // extract and load all plugins under `assets/www/plugins`
+    match std::fs::read_dir("assets/www/plugins") {
+        Ok(dir) => dir
+            .filter_map(|x| x.ok())
+            .filter(|x| x.path().is_file())
+            .filter(|x| x.path().extension().is_some_and(|ext| ext == "zip"))
+            .filter(|x| x.path().file_stem().is_some_and(|stem| stem != "api"))
+            .for_each(|x| {
+                let name = x
+                    .path()
+                    .file_stem()
+                    .expect("this cannot happen")
+                    .to_string_lossy()
+                    .into();
+                match load_plugin(x.path(), &serve_path, &name) {
+                    Ok(_) => log::info!("loaded plugin {}", name),
+                    Err(err) => log::error!("failed to load plugin {}, {}", name, err),
+                }
+            }),
+        Err(err) => {
+            log::error!("failed to read plugin directory: {}", err);
+        }
+    };
+
+    let app = Router::new()
+        .route("/api/adapters", get(api::adapters))
+        .route("/api/files/unzip", post(api::unzip))
+        .route("/api/files/dir", post(api::dir))
+        .route("/api/files/ls", post(api::dir))
+        .route("/api/files/config/load", post(api::load_config))
+        .route("/api/files/config/save", post(api::save_config))
+        .route("/api/models/list", get(api::models))
+        .route("/api/models/info", get(api::info))
+        .route("/api/models/state", get(api::state))
+        .route("/api/models/load", post(api::load))
+        .route("/api/models/unload", get(api::unload))
+        .route("/api/oai/models", get(oai::models))
+        .route("/api/oai/v1/models", get(oai::models))
+        .route("/api/oai/completions", post(oai::completions))
+        .route("/api/oai/v1/completions", post(oai::completions))
+        .route("/api/oai/chat/completions", post(oai::chat_completions))
+        .route("/api/oai/v1/chat/completions", post(oai::chat_completions))
+        .route("/api/oai/embeddings", post(oai::embeddings))
+        .route("/api/oai/v1/embeddings", post(oai::embeddings))
+        .fallback_service(ServeDir::new(serve_path))
+        .layer(CorsLayer::permissive())
+        .with_state(ThreadState(sender));
+
+    let addr = SocketAddr::from((args.ip.unwrap_or(Ipv4Addr::new(0, 0, 0, 0)), args.port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    log::info!("server started at {addr}");
+    axum::serve(listener, app).await;
+
+    Ok(())
+}
+
+#[tokio::main]
+async  fn main() {
+
+
+    let tauri_task = tokio::spawn(main2());
+
+
+
     let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
     let system_tray_menu = SystemTrayMenu::new().add_item(quit);
     tauri::Builder::default()
@@ -616,112 +738,4 @@ pub fn tauri_main() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-
-#[derive(Parser, Debug, Clone)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(long, short, value_name = "FILE")]
-    config: Option<PathBuf>,
-    #[arg(long, short)]
-    ip: Option<Ipv4Addr>,
-    #[arg(long, short, default_value_t = 65530)]
-    port: u16,
-}
-
-#[tokio::main]
-async fn main() {
-    simple_logger::SimpleLogger::new()
-        .with_level(log::LevelFilter::Warn)
-        .with_module_level("ai00_server", log::LevelFilter::Trace)
-        .init()
-        .unwrap();
-
-    let args = Args::parse();
-    let (sender, receiver) = flume::unbounded::<ThreadRequest>();
-    tokio::task::spawn_blocking(move || model_route(receiver));
-
-    {
-        let path = args
-            .config
-            .clone()
-            .unwrap_or("assets/configs/Config.toml".into());
-        log::info!("reading config {}...", path.to_string_lossy());
-
-        let request = load_config(path).expect("load config failed").into();
-        let _ = sender.send(ThreadRequest::Reload {
-            request,
-            sender: None,
-        });
-    }
-
-    let serve_path = {
-        let path = tempfile::tempdir()
-            .expect("create temp dir failed")
-            .into_path();
-        load_web("assets/www/index.zip", &path).expect("load frontend failed");
-        path
-    };
-
-    let _=tauri_main();
-
-    // create `assets/www/plugins` if it doesn't exist
-    if !Path::new("assets/www/plugins").exists() {
-        fs::create_dir("assets/www/plugins").expect("create plugins dir failed");
-    }
-
-    // extract and load all plugins under `assets/www/plugins`
-    match std::fs::read_dir("assets/www/plugins") {
-        Ok(dir) => dir
-            .filter_map(|x| x.ok())
-            .filter(|x| x.path().is_file())
-            .filter(|x| x.path().extension().is_some_and(|ext| ext == "zip"))
-            .filter(|x| x.path().file_stem().is_some_and(|stem| stem != "api"))
-            .for_each(|x| {
-                let name = x
-                    .path()
-                    .file_stem()
-                    .expect("this cannot happen")
-                    .to_string_lossy()
-                    .into();
-                match load_plugin(x.path(), &serve_path, &name) {
-                    Ok(_) => log::info!("loaded plugin {}", name),
-                    Err(err) => log::error!("failed to load plugin {}, {}", name, err),
-                }
-            }),
-        Err(err) => {
-            log::error!("failed to read plugin directory: {}", err);
-        }
-    };
-
-    let app = Router::new()
-        .route("/api/adapters", get(api::adapters))
-        .route("/api/files/unzip", post(api::unzip))
-        .route("/api/files/dir", post(api::dir))
-        .route("/api/files/ls", post(api::dir))
-        .route("/api/files/config/load", post(api::load_config))
-        .route("/api/files/config/save", post(api::save_config))
-        .route("/api/models/list", get(api::models))
-        .route("/api/models/info", get(api::info))
-        .route("/api/models/state", get(api::state))
-        .route("/api/models/load", post(api::load))
-        .route("/api/models/unload", get(api::unload))
-        .route("/api/oai/models", get(oai::models))
-        .route("/api/oai/v1/models", get(oai::models))
-        .route("/api/oai/completions", post(oai::completions))
-        .route("/api/oai/v1/completions", post(oai::completions))
-        .route("/api/oai/chat/completions", post(oai::chat_completions))
-        .route("/api/oai/v1/chat/completions", post(oai::chat_completions))
-        .route("/api/oai/embeddings", post(oai::embeddings))
-        .route("/api/oai/v1/embeddings", post(oai::embeddings))
-        .fallback_service(ServeDir::new(serve_path))
-        .layer(CorsLayer::permissive())
-        .with_state(ThreadState(sender));
-
-    let addr = SocketAddr::from((args.ip.unwrap_or(Ipv4Addr::new(0, 0, 0, 0)), args.port));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    log::info!("server started at {addr}");
-    axum::serve(listener, app).await.unwrap();
-    
 }
